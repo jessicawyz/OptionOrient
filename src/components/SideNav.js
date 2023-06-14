bc00075d955d9e2f8462526f2773e020962210fa
Yiyang <liuyiyang0929@gmail.com>
2023-06-14 23:26:31 +0800

    minor layout fix

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -5,7 +5,7 @@ export default function SideNav() {
     const navigate = useNavigate();
     return (
         <div className="sideNav">
-              <div className="nav">Home</div>
+              <Link to='/home' className="nav"><div>Home</div></Link>
               <Link to="/notFound" className="nav"><div className="nav">Forum</div></Link>
               <Link to="/notFound" className="nav"><div className="nav">Friends</div></Link>
               <Link to="/notFound" className="nav"><div className="nav">Chats</div></Link>
