8c695f8bcfd9abcb05e805d07d37cf88afc02e99
Yiyang <liuyiyang0929@gmail.com>
2023-07-24 00:33:24 +0800

    Edited profile UI

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,6 +6,7 @@ import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { doc, updateDoc } from 'firebase/firestore';
 import { updateProfile, getAuth } from "firebase/auth";
 import Avatar from "@mui/material/Avatar";
+import '../css/Profile.css'
 
 const Profile = () => {
   const { user, logout } = UserAuth();
@@ -75,44 +76,49 @@ const Profile = () => {
     return (
         <div>
           {userData ? (
-            <div>
-              <h2 className="tw-text-2xl tw-font-bold tw-py-2 tw-text-white">Profile</h2>
+            <div className='tw-m-4'>
     
-              <div className="tw-flex tw-items-center tw-mb-8">
-                <div>
-                  <Avatar alt="Profile" sx={{ width: 100, height: 100 }} src={photoURL} className="tw-my-8"/>
+              <div className="tw-flex tw-flex-row tw-mb-8">
+                <div className='tw-flex tw-w-1/4 tw-flex-col tw-items-start'>
+                  <Avatar alt="Profile" sx={{ width: 150, height: 150 }} src={photoURL} className="tw-my-8"/>
                   {/* Profile picture upload button */}
-                  <input type="file" accept="image/*" onChange={handleProfilePictureUpload} />
+                  <label className="profileButton tw-p-3 tw-px-6 tw-text-white tw-text-lg tw-rounded-md hover:tw-opacity-90">
+                    <input type="file" accept="image/*" onChange={handleProfilePictureUpload} />
+                    Upload Photo
+                  </label>
                 </div>
     
-                <div>
+                <div className="tw-m-8">
                   {/* Display the username */}
-                  <p className="tw-font-medium tw-text-white">Username: {userData.username}</p>
+                  <p className="tw-font-semibold tw-text-2xl tw-text-white tw-mb-6 tw-tracking-wide">Username: {userData.username}</p>
                   {/* Display the email */}
-                  <p className="tw-font-medium tw-text-white">Email: {userData.email}</p>
+                  <p className="tw-font-medium tw-text-xl tw-text-white">Email: {userData.email}</p>
+                  <div className="tw-flex tw-w-max tw-my-8">
+                    {/* Logout button */}
+                    <button
+                      className="profileButton tw-flex-grow tw-my-4 tw-me-10 tw-p-3 tw-px-14 tw-text-white tw-text-lg tw-rounded-md hover:tw-opacity-90"
+                      onClick={handleLogout}
+                    >
+                      Logout
+                    </button>
+              
+                    {/* Home button */}
+                    <Link
+                      to="/home"
+                      className="profileButton tw-flex-grow tw-my-4 tw-me-4 tw-p-3 tw-px-14 tw-text-white tw-text-lg tw-rounded-md hover:tw-opacity-90"
+                    >
+                      Home
+                    </Link>
+                  </div>
+                  </div>
                 </div>
               </div>
-            </div>
           ) : (
             <p>Loading...</p>
           )}
+          </div>
     
-          {/* Logout button */}
-          <button
-            className="tw-border tw-border-gray-800 tw-bg-gray-800 hover:tw-bg-gray-600 tw-w-20 tw-p-4 tw-mt-auto tw-text-white"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
-    
-          {/* Home button */}
-          <Link
-            to="/home"
-            className="tw-border tw-border-gray-800 tw-bg-gray-800 hover:tw-bg-gray-600 tw-w-20 tw-p-4 tw-mt-4 tw-text-white"
-          >
-            Home
-          </Link>
-        </div>
+          
       );
     };
     
